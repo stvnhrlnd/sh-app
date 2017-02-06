56d4f18be633a03bbf1614eefafb870b1aca758f
Steven <steven_harland@hotmail.co.uk>
2017-02-06 20:46:27 +0000

    Update content service
    Add method which loads content from storage into an instance variable,
    called when the service is created.
    
    Add methods for querying content using JSONPath.

--- app/content.service.ts
+++ app/content.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import * as jp from 'jsonpath';
+
+import { Content } from './content';
 import { StorageService } from './storage.service';
 
 @Injectable()
 export class ContentService {
+    private content: Content[];
+
     constructor(private storageService: StorageService) {
+        this.loadContent();
+    }
+
+    contentAtJSONPath(pathExpression: string): Content[] {
+        let content = jp.query(this.content, pathExpression);
+        return content;
+    }
+
+    contentSingleAtJSONPath(pathExpression: string): Content {
+        let content = this.contentAtJSONPath(pathExpression);
+        return content[0];
+    }
+
+    loadContent() {
+        this.content = JSON.parse(this.storageService.getItem('content'));
     }
 }
